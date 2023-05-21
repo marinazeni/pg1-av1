f2a6f83bb02815ca469a737350242031184f0597
marinazeni <marina_zeni@hotmail.com>
2023-05-21 18:45:23 -0300

    funciona

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "unity/Build/Build.loader.js",
+    dataUrl: "unity/Build/Build.data",
+    frameworkUrl: "unity/Build/Build.framework.js",
+    codeUrl: "unity/Build/Build.wasm",
+  });
+
+  return <Unity unityProvider={unityProvider} />;
 }
 
 export default App;
